393d5d44720fe2385bb1ba6d1d9fa54b4bd4d9c1
Dina Nashaat <dina.nashaat@gmail.com>
2016-06-09 15:54:16 +0200

    Structure the Controller

--- mips/src/Controller.vhd
+++ mips/src/Controller.vhd
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 
 entity Controller is -- MultiCycle Controller
-	port(clk: in STD_logic;
+	port(clk, reset: in STD_logic;
 	op, funct: in STD_logic_vector (5 downto 0);
 	zero: in STD_logic;
 	IorD, MemWrite, IRWrite, RegWrite: out STd_logic;
@@ -18,3 +18,29 @@ end;
 architecture struct of Controller is
 signal ALUOp: STD_logic_vector (1 downto 0);
 signal PCWrite, Branch: STD_logic;
+
+component ALUDec 
+	port(
+	funct: in STD_logic_vector (5 downto 0);
+	ALUOp: in STD_logic_vector (1 downto 0);
+	ALUControl: out STD_logic_vector (2 downto 0)
+	);	 
+end component;									
+
+component MainDec
+	port(		
+	op: in STD_Logic_vector (5 downto 0);
+	IorD, MemWrite, IRWrite, RegWrite: out STd_logic;
+	PCSrc: out STD_logic_vector (1 downto 0);
+	ALUSrcB: out STD_logic_vector (1 downto 0);
+	ALUSrcA: out STD_logic;
+	RegDst, MemtoReg: out STD_logic
+	);				   
+end component;
+
+begin
+	PCEn <= (Branch AND zero) OR PCWrite;
+	
+	md: MainDec port map (op, IorD, MemWrite, IRWrite, RegWrite, PCSrc, ALUSrcB, ALUSrcA, RegDst, MemtoReg);
+	ad: ALUDec port map (funct, ALUOp, ALUControl);
+end;
